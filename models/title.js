c3563b63cce0b373b302a14aa0de6e64440c1230
Matthew <mgmenegus@gmail.com>
2018-04-22 16:09:25 -0400

    commit

---
+++ models/title.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var articleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+
+  link: {
+    type: String,
+    required: true
+  },
+
+ // note: {
+   // type: Schema.Types.ObjectId,
+    //ref: "Note"
+  //}
+});
+
+var article = mongoose.model("article", articleSchema);
+
+module.exports = article;
